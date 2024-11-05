ae900127260716c5276007428bbdbaaedec45fc6
Gabriele Tassoni <g.tassoni@bancolini.com>
2024-11-05 16:57:33 +0000

    Clean up the ERD and regenerate it

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_18_153622) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_28_152811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,83 +69,73 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_18_153622) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "categories", force: :cascade do |t|
+  create_table "affiliations", force: :cascade do |t|
     t.string "name"
-    t.text "description"
+    t.bigint "army_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["description"], name: "index_categories_on_description"
+    t.index ["army_id"], name: "index_affiliations_on_army_id"
+    t.index ["name", "army_id"], name: "index_affiliations_on_name_and_army_id", unique: true
+  end
+
+  create_table "armies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_armies_on_name", unique: true
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
-  create_table "charts", force: :cascade do |t|
+  create_table "entity_modifiers", force: :cascade do |t|
+    t.bigint "modifier_id", null: false
+    t.string "entity_type", null: false
+    t.bigint "entity_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["entity_type", "entity_id"], name: "index_entity_modifiers_on_entity"
+    t.index ["modifier_id"], name: "index_entity_modifiers_on_modifier_id"
+  end
+
+  create_table "modifier_relationships", force: :cascade do |t|
+    t.bigint "modifier_id", null: false
+    t.bigint "related_modifier_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["modifier_id"], name: "index_modifier_relationships_on_modifier_id"
+    t.index ["related_modifier_id"], name: "index_modifier_relationships_on_related_modifier_id"
+  end
+
+  create_table "modifiers", force: :cascade do |t|
     t.string "name"
+    t.decimal "mov", precision: 3, scale: 1, default: "0.0"
+    t.integer "ini", default: 0
+    t.integer "att", default: 0
+    t.integer "str", default: 0
+    t.integer "def", default: 0
+    t.integer "res", default: 0
+    t.integer "aim", default: 0
+    t.integer "cou", default: 0
+    t.integer "fea", default: 0
+    t.integer "dis", default: 0
+    t.integer "pow", default: 0
+    t.integer "rank", default: 0
+    t.integer "wounds", default: 0
+    t.integer "force", default: 0
+    t.integer "cost", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "chart_library_name"
-    t.index ["chart_library_name"], name: "index_charts_on_chart_library_name", unique: true
-    t.index ["name"], name: "index_charts_on_name", unique: true
-  end
-
-  create_table "collected_data", force: :cascade do |t|
-    t.bigint "thing_id", null: false
-    t.bigint "user_id", null: false
-    t.jsonb "json_data"
-    t.datetime "injested_at"
-    t.decimal "latitude", precision: 10, scale: 6
-    t.decimal "longitude", precision: 10, scale: 6
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "data_configuration_id"
-    t.text "address"
-    t.index ["address"], name: "index_collected_data_on_address"
-    t.index ["data_configuration_id"], name: "index_collected_data_on_data_configuration_id"
-    t.index ["latitude", "longitude"], name: "index_collected_data_on_latitude_and_longitude"
-    t.index ["thing_id"], name: "index_collected_data_on_thing_id"
-    t.index ["user_id"], name: "index_collected_data_on_user_id"
-  end
-
-  create_table "custom_dashboard_data_configurations", force: :cascade do |t|
-    t.bigint "custom_dashboard_id", null: false
-    t.bigint "data_configuration_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["custom_dashboard_id"], name: "idx_on_custom_dashboard_id_a3eb23159d"
-    t.index ["data_configuration_id"], name: "idx_on_data_configuration_id_1163724a13"
-  end
-
-  create_table "custom_dashboards", force: :cascade do |t|
-    t.string "name"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["name"], name: "index_custom_dashboards_on_name"
-    t.index ["user_id"], name: "index_custom_dashboards_on_user_id"
-  end
-
-  create_table "data_configuration_timespans", force: :cascade do |t|
-    t.bigint "data_configuration_id", null: false
-    t.bigint "timespan_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["data_configuration_id"], name: "index_data_configuration_timespans_on_data_configuration_id"
-    t.index ["timespan_id"], name: "index_data_configuration_timespans_on_timespan_id"
-  end
-
-  create_table "data_configurations", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.string "name"
-    t.boolean "visible"
-    t.boolean "compare_with_same_category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "chart_id"
-    t.string "code"
-    t.text "description"
-    t.index ["chart_id"], name: "index_data_configurations_on_chart_id"
-    t.index ["code", "user_id"], name: "index_data_configurations_on_code_and_user_id", unique: true
-    t.index ["description"], name: "index_data_configurations_on_description"
-    t.index ["name"], name: "index_data_configurations_on_name"
+    t.bigint "category_id", null: false
+    t.integer "character_cost"
+    t.string "special_cost"
+    t.index ["category_id"], name: "index_modifiers_on_category_id"
+    t.index ["name"], name: "index_modifiers_on_name", unique: true
+    t.index ["special_cost"], name: "index_modifiers_on_special_cost"
   end
 
   create_table "permission_roles", force: :cascade do |t|
@@ -178,6 +168,30 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_18_153622) do
     t.index ["name"], name: "index_predicates_on_name", unique: true
   end
 
+  create_table "profiles", force: :cascade do |t|
+    t.decimal "mov", precision: 3, scale: 1
+    t.integer "ini", default: 0
+    t.integer "att", default: 0
+    t.integer "str", default: 0
+    t.integer "def", default: 0
+    t.integer "res", default: 0
+    t.integer "aim", default: 0
+    t.integer "cou", default: 0
+    t.integer "fea", default: 0
+    t.integer "dis", default: 0
+    t.integer "pow", default: 0
+    t.integer "rank", default: 0
+    t.integer "wounds", default: 0
+    t.integer "force", default: 0
+    t.integer "cost", default: 0
+    t.bigint "unit_id", null: false
+    t.bigint "affiliation_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["affiliation_id"], name: "index_profiles_on_affiliation_id"
+    t.index ["unit_id"], name: "index_profiles_on_unit_id"
+  end
+
   create_table "role_users", force: :cascade do |t|
     t.bigint "role_id", null: false
     t.bigint "user_id", null: false
@@ -208,8 +222,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_18_153622) do
     t.string "kind", default: "string", null: false
     t.string "ns", default: "main"
     t.string "key", null: false
-    t.float "latitude"
-    t.float "longitude"
     t.text "raw"
     t.string "label"
     t.datetime "created_at", null: false
@@ -218,28 +230,14 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_18_153622) do
     t.index ["ns", "key"], name: "index_thecore_settings_on_ns_and_key", unique: true
   end
 
-  create_table "things", force: :cascade do |t|
+  create_table "units", force: :cascade do |t|
     t.string "name"
-    t.bigint "user_id", null: false
-    t.text "description"
+    t.bigint "army_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "category_id", null: false
-    t.index ["category_id"], name: "index_things_on_category_id"
-    t.index ["description"], name: "index_things_on_description"
-    t.index ["name"], name: "index_things_on_name", unique: true
-    t.index ["user_id"], name: "index_things_on_user_id"
-  end
-
-  create_table "timespans", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "duration"
-    t.text "description"
-    t.index ["description"], name: "index_timespans_on_description"
-    t.index ["duration"], name: "index_timespans_on_duration"
-    t.index ["name"], name: "index_timespans_on_name", unique: true
+    t.boolean "character", default: false, null: false
+    t.index ["army_id"], name: "index_units_on_army_id"
+    t.index ["name"], name: "index_units_on_name", unique: true
   end
 
   create_table "used_tokens", force: :cascade do |t|
@@ -266,31 +264,25 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_18_153622) do
     t.boolean "admin", default: false, null: false
     t.bigint "lock_version"
     t.boolean "locked", default: false, null: false
-    t.string "encrypted_access_token"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["encrypted_access_token"], name: "index_users_on_encrypted_access_token"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "collected_data", "data_configurations"
-  add_foreign_key "collected_data", "things"
-  add_foreign_key "collected_data", "users"
-  add_foreign_key "custom_dashboard_data_configurations", "custom_dashboards"
-  add_foreign_key "custom_dashboard_data_configurations", "data_configurations"
-  add_foreign_key "custom_dashboards", "users"
-  add_foreign_key "data_configuration_timespans", "data_configurations"
-  add_foreign_key "data_configuration_timespans", "timespans"
-  add_foreign_key "data_configurations", "charts"
-  add_foreign_key "data_configurations", "users"
+  add_foreign_key "affiliations", "armies"
+  add_foreign_key "entity_modifiers", "modifiers"
+  add_foreign_key "modifier_relationships", "modifiers"
+  add_foreign_key "modifier_relationships", "modifiers", column: "related_modifier_id"
+  add_foreign_key "modifiers", "categories"
   add_foreign_key "permission_roles", "permissions"
   add_foreign_key "permission_roles", "roles"
   add_foreign_key "permissions", "actions"
   add_foreign_key "permissions", "predicates"
   add_foreign_key "permissions", "targets"
+  add_foreign_key "profiles", "affiliations"
+  add_foreign_key "profiles", "units"
   add_foreign_key "role_users", "roles"
   add_foreign_key "role_users", "users"
-  add_foreign_key "things", "categories"
-  add_foreign_key "things", "users"
+  add_foreign_key "units", "armies"
   add_foreign_key "used_tokens", "users"
 end
